4abf76aa287ce2d67a3a66d1d5ac4faa796b7b5f
Divine Evna Olong <92402915+Dipraise1@users.noreply.github.com>
2024-05-11 09:25:02 +0100

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import GridEg from './../components/gridEg/index';
+import HorizontalEg from './../components/horizontalEg/index';
+import './../styles/App.css';
+import GridSwap from './gridSwapEg';
+
+function App() {
+  return (
+    <div className={['App'].join(' ')}>
+      <div className='appContainer'>
+        <div>
+     
+        </div>
+        
+        <div className='grid-egs'>
+          <div>
+         
+           
+          </div>
+          <div style={{marginTop: '48px'}}>
+          
+            <GridSwap />
+            <GridEg />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
